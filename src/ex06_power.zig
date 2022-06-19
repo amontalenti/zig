2b1cf761528a8650ad2b72aff25ffbf9a28d3cae
Andrew Montalenti <andrew@parsely.com>
2022-06-19 09:55:35 -0400

    Improve style of ex06

--- src/ex06_power.zig
+++ src/ex06_power.zig
@@ -19,11 +19,11 @@ pub fn power(base: u64, n: u64) u64 {
 // directly; in Zig, we need to do a no-performance-cost var declaration to
 // make it clear that we are modifying a variable which merely "defaults"
 // to the passed-in fn argument value.
-pub fn powerN(base: u64, n: u64) u64 {
+pub fn powerDown(base: u64, n: u64) u64 {
     var p: u64 = 1;
-    var _n = n;
+    var n_ = n;
 
-    while (_n > 0) : (_n -= 1) {
+    while (n_ > 0) : (n_ -= 1) {
         p = p * base;
     }
 
@@ -34,19 +34,19 @@ pub fn main() !void {
     var i: u64 = 1;
 
     while (i <= 10) : (i += 1) {
-        print("{d} = {d} = {d}\n", .{ i, power(2, i), powerN(2, i) });
+        print("{d} = {d} = {d}\n", .{ i, power(2, i), powerDown(2, i) });
     }
 }
 
 // We can convert our debug output into a nice unit test. This compares the
-// return values of power() to powerN().
+// return values of power() to powerDown().
 test {
     const expectEqual = std.testing.expectEqual;
     var i: u64 = 1;
     // change maxPower to e.g. 100 and it'll overflow
-    const maxPower = 10;
+    const max_power = 10;
 
-    while (i <= maxPower) : (i += 1) {
-        try expectEqual(power(2, i), powerN(2, i));
+    while (i <= max_power) : (i += 1) {
+        try expectEqual(power(2, i), powerDown(2, i));
     }
 }
